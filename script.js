25f702d721ee05c5ac9faef79a31e69fd6dbdb9c
Karan <Karan@gmail.com>
2026-02-23 12:09:48 +1200

    b

--- script.js
+++ script.js
@@ -24,4 +24,7 @@ app.use("/User",require("./Routes/User").Routers)
 app.use("/Delete",require("./Routes/deleteItems").Routers)
 app.use("/ProductsByCategory",require("./Routes/ProductsByCategory").Routers)
 
-app.listen(5000)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
